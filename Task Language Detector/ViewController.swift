44f71702c2d7015f67adf7074e43dc173a88f38e
Roman <cold.steel.br@gmail.com>
2018-07-10 13:54:13 +0700

    Initial Commit

---
+++ Task Language Detector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task Language Detector
+//
+//  Created by Roman Brazhnikov on 10.07.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
